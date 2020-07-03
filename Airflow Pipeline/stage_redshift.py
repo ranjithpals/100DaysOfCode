ce83e0881e563c05c0bf68e404265ee197eac3f8
Ranjith Gonugunta <39640906+ranjithpals@users.noreply.github.com>
2020-07-03 17:39:29 -0400

    Day #53 updates

--- Airflow Pipeline/stage_redshift.py
+++ Airflow Pipeline/stage_redshift.py
@@ -4,6 +4,17 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class StageToRedshiftOperator(BaseOperator):
+    template_fields = ("s3_key",)
+    copy_sql = """
+        COPY {}
+        FROM '{}'
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        IGNOREHEADER {}
+        FORMAT AS JSON '{}'
+        TIMEFORMAT AS 'epochmillisecs'
+        region 'us-west-2'
+    """
     ui_color = '#358140'
 
     @apply_defaults
@@ -20,36 +31,38 @@ class StageToRedshiftOperator(BaseOperator):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
         # Map params here
-        self.redshift_conn_id = redshift_conn_id,
-        self.aws_credentials_id = aws_credentials_id,
-        self.table = table,
-        self.s3_bucket = s3_bucket,
-        self.s3_key = s3_key,
-        self.delimiter = delimiter,
+        self.redshift_conn_id = redshift_conn_id
+        self.aws_credentials_id = aws_credentials_id
+        self.table = table
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.delimiter = delimiter
         self.ignore_headers = ignore_headers
 
     def execute(self, context):
         # self.log.info('StageToRedshiftOperator not implemented yet')
         aws_hook = AwsHook(self.aws_credentials_id)
-        postgres_hook = PostgresHook(self.redshift_conn_id)
         credentials = aws_hook.get_credentials()
+        postgres_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
         
         self.log.info("Deleting existing data if available in Redshift tables")
         postgres_hook.run("DELETE FROM {}".format(self.table))
         
-        
         self.log.info("Copying data from S3 bucket to Redshift")
         rendered_key = self.s3_key.format(**context)
+        self.log.info(rendered_key)
         s3_path = "s3://{}/{}".format(self.s3_bucket, rendered_key)
-        formatted_sql = "COPY {} FROM '{}' \
-                          ACCESS_KEY_ID '{{}}' \
-                          SECRET_ACCESS_KEY '{{}}' \
-                          DELIMITER '{}' \
-                          IGNOREHEADER {}".format(self.table, s3_path, credentials.access_key, credentials.secret_key, self.delimiter, self.ignore_headers)
+        formatted_sql = StageToRedshiftOperator.copy_sql.format(
+            self.table, 
+            s3_path, 
+            credentials.access_key, 
+            credentials.secret_key, 
+            self.ignore_headers,
+            'auto',
+        )
         
         postgres_hook.run(formatted_sql)
-        
-        pass
         
         
 
